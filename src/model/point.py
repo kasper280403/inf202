f595b7004279a3e47be4786b879597823e375194
Fredrik Tveter <fredrik.tveter@nmbu.no>
2026-01-06 14:00:44 +0100

    feat: Point class added

---
+++ src/model/point.py
@@ -0,0 +1,24 @@
+class Point:
+    def __init__(self,cord):
+        self._x = cord[0]
+        self._y = cord[1]
+        self._id = id
+
+    def get_coordinates(self):
+        return([self._x,self._y])
+    
+    def get_ycoordinates(self):
+        return(self._y)
+    
+    def get_xcoordinates(self):
+        return(self._x)
+    
+    def get_id(self):
+        return(self._id)
+    
+    def __hash__(self):
+        hash((self._x,self._y))
+
+    def __str__(self):
+        return(f'Point with coordinates x = {self._x}, y = {self._y}')
+    
