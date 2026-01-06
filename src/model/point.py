e15a952afbdac9845ea2067a7078e7ef9da6dd14
Fredrik Tveter <fredrik.tveter@nmbu.no>
2026-01-06 14:11:02 +0100

    docs: added some docstrings to point class

--- src/model/point.py
+++ src/model/point.py
@@ -1,16 +1,25 @@
 class Point:
+    '''
+    Represents a point
+
+    Attributes:
+        x (int) x coordinate of this point
+        y (int) y coordinate of this point
+    '''
     def __init__(self,cord):
         self._x = cord[0]
         self._y = cord[1]
-        self._id = id
 
-    def get_coordinates(self):
+    def get_coordinates(self) -> list[float]:
+        '''Return a list with the x and y coordinate'''
         return([self._x,self._y])
     
-    def get_ycoordinates(self):
+    def get_ycoordinates(self) -> float:
+        '''Return y coordinate of point'''
         return(self._y)
     
-    def get_xcoordinates(self):
+    def get_xcoordinates(self) -> float:
+        '''Return x coordinate of point'''
         return(self._x)
     
     def __hash__(self):
