e60b9f38ed610eb6c39d8a8ef91a96636578b01f
kasper280403 <kasper.s.karlsen@gmail.com>
2026-01-06 13:06:06 +0100

    feat: Added getter and setter for midpoint

--- src/model/cells/triangle.py
+++ src/model/cells/triangle.py
@@ -10,10 +10,11 @@ class Triangle(Cell):
         corner_point_ids (list[int]): Corner points of the cell.
         neighbor_ids (list[int]): IDs of bordering cells.
     """
-    def __init__(self, id, corner_point_ids, neighbor_ids):
+    def __init__(self, id, corner_point_ids, neighbor_ids, midpoint):
         super().__init__(id)
         super().__init__(corner_point_ids)
         super().__init__(neighbor_ids)
+        self.midpoint = -1
 
 
     def check_neighbour(self, test_corner_point_ids):
@@ -29,4 +30,27 @@ class Triangle(Cell):
             bool: True if the cell is bordering itself
         """
         return len(set(self.corner_point_ids) & set(test_corner_point_ids)) == 2
+
+    def get_midpoint(self):
+        if self.midpoint == -1:
+            self.set_midpoint()
+
+        return self.midpoint
+
+
+    def set_midpoint(self):
+        x_coordinates = []
+        y_coordinates = []
+
+        for point in self.corner_point_ids:
+            coordinates = Point.get_coordinates(point)
+            x_coordinates.append(coordinates[0])
+            y_coordinates.append(coordinates[1])
+
+        x_mid = 1/3 * sum(x_coordinates)
+        y_mid = 1/3 * sum(y_coordinates)
+
+        self.midpoint =  [x_mid, y_mid]
+
+
 
