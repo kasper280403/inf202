268cfacaec601a8b961bbfecb0214a6eab8fba24
Fredrik Tveter <fredrik.tveter@nmbu.no>
2026-01-07 12:56:21 +0100

    refactor: changed to class CreateImage

--- src/model/view/createImage.py
+++ src/model/view/createImage.py
@@ -1,39 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+class CreateImage():
+    def __init__(self,TriangleList):
+        self._triangles = TriangleList #get a list of triangles can be done multiple ways
+        self._umax = 1
+        self._umin = 0
+        self._u = 0.0
 
-umax = 1
-umin = 0
+    def plot_Triangles(self):# Plot the mesh by adding all triangles with their value
+        plt.figure()
 
-u = 0.0 # used for different colors
+         # Create the colormap
+        sm = plt.cm.ScalarMappable(cmap='viridis')
+        sm.set_array([self._umin,self._umin])
 
-Triangles = Mesh.get_triangles() #get a list of triangles can be done multiple ways
-# Plot the mesh by adding all triangles with their value
-plt.figure()
+        cbar_ax = plt.gca().inset_axes([1, 0, 0.05, 1])  # adjust position and size as needed
+        plt.colorbar(sm, cax=cbar_ax, label='c')
 
-# Create the colormap
-sm = plt.cm.ScalarMappable(cmap='viridis')
-sm.set_array([umin,umax])
+        for i in self._triangles:
+            plt.gca().add_patch(plt.Polygon(i.get_coordinates(), color=plt.cm.viridis((u - umin)/(umax - umin)), alpha=0.9))
+        # Add labels to axes
+        plt.xlabel('a')
+        plt.ylabel('b')
 
+        plt.xlim(0, 1)  # set the x-axis limits
+        plt.ylim(0, 1)  # set the y-axis limits
+        plt.gca().set_aspect('equal')
 
-
-# Add colorbar using a separate axis
-cbar_ax = plt.gca().inset_axes([1, 0, 0.05, 1])  # adjust position and size as needed
-plt.colorbar(sm, cax=cbar_ax, label='c')
-for i in Triangles:
-    plt.gca().add_patch(plt.Polygon(i.get_coordinates(), color=plt.cm.viridis((u - umin)/(umax - umin)), alpha=0.9))
-
-
-
-# Add labels to axes
-plt.xlabel('a')
-plt.ylabel('b')
-
-plt.xlim(0, 1)  # set the x-axis limits
-plt.ylim(0, 1)  # set the y-axis limits
-plt.gca().set_aspect('equal')
-
-# Show plot
-plt.savefig("img.png")
-
-plt.close()
+    def save_img(self,file_loc):
+        # Show plot
+        plt.savefig(file_loc)
+        
