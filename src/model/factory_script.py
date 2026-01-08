18032a4c1c5172f3e179a7fe93210a77e2756979
kasper280403 <kasper.s.karlsen@gmail.com>
2026-01-08 11:09:52 +0100

    feat: added create point method

--- src/model/factory_script.py
+++ src/model/factory_script.py
@@ -12,16 +12,6 @@ lines = mesh.cells_dict["line"]
 
 
 
-     
-# Should become a method------
-points = mesh.points
-
-point_list = []
-for point in points:
-    x = Point(point)
-    point_list.append(x)
-#-----------------------------
-
 
 '''
 Mål: 
@@ -45,6 +35,7 @@ class Factory:
     def __init__(self, mesh_name):
         self.mesh = meshio.read(Path(__file__).parent.parent/mesh_name)
         self.triangle_list = None
+        self.point_list = None
 
     def get_point_list(self):
         return self.point_list
@@ -53,15 +44,28 @@ class Factory:
         return self.triangle_list
 
     def create_triangles(self):
+
+        if self.point_list is None:
+            self.create_points()
         triangles = self.mesh.cells_dict["triangle"]
         triangle_list = []
 
         for triangle in triangles:
             l = []
             for point_id in triangle:
-                l.append(point_list[point_id])
+                l.append(self.point_list[point_id])
             x = Triangle(l)
             triangle_list.append(x)
 
         self.triangle_list = triangle_list
+
+    def create_points(self):
+        points = mesh.points
+
+        point_list = []
+        for point in points:
+            x = Point(point)
+            point_list.append(x)
+
+        self.point_list = point_list
     
