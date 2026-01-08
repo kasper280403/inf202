5ad7a6edaaa6606614ceb7326e17be6438b176b1
Fredrik Tveter <fredrik.tveter@nmbu.no>
2026-01-08 13:59:47 +0100

    feat: updated main and fixed imports

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+from model.cells.triangle import Triangle
+from model.factory.factory import Factory
+
 import pathlib
 import meshio
 mesh_path = pathlib.Path(__file__).parent / "resources" / "bay.msh"
@@ -6,5 +9,7 @@ msh = meshio.read(mesh_path) #read the mesh
 
 points = msh.points #get all the points in the mesh
 cells = msh.cells #connect the cell structure
+triangle_list = []
 
+    
 
