7f618cc0efb287efdb653ce99f67f1d6dfb3abef
Fredrik Tveter <fredrik.tveter@nmbu.no>
2026-01-08 13:37:00 +0100

    feat: read mesh

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 import pathlib
-
+import meshio
 mesh_path = pathlib.Path(__file__).parent / "resources" / "bay.msh"
+
+msh = meshio.read(mesh_path) #read the mesh
+
+points = msh.points #get all the points in the mesh
+cells = msh.cells #connect the cell structure
+
 
