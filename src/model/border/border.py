43df9f0acace66539257e61175f10d956153c56f
Fredrik Tveter <fredrik.tveter@nmbu.no>
2026-01-14 12:20:07 +0100

    fix: fixed bug related to normals at border triangles

--- src/model/border/border.py
+++ src/model/border/border.py
@@ -23,11 +23,10 @@ class Border:
     def calculate_normal(self):
         line_vec = self.p2.get_coordinates() - self.p1.get_coordinates()
         normal = np.cross(line_vec, [0, 0, 1])[0:2]
-        if self.neighbour is not None:
-            midt_p1 = self.neighbour.get_midpoint() - self.p1.get_coordinates()
-            theta = np.arccos(np.inner(normal, midt_p1) / (np.linalg.norm(normal) * np.linalg.norm(midt_p1)))
-            if theta > np.pi / 2:
-                normal = normal * (-1)
+        midt_p1 = self.p1.get_coordinates() - self.triangle.get_midpoint() 
+        theta = np.arccos(np.inner(normal, midt_p1) / (np.linalg.norm(normal) * np.linalg.norm(midt_p1)))
+        if theta > np.pi / 2:
+            normal = normal * (-1)
         return normal
 
     def get_points(self):
