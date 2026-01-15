4e96d45dc11277cb0e41602d174332d5356f5f40
Fredrik Tveter <fredrik.tveter@nmbu.no>
2026-01-15 13:15:58 +0100

    fix: fixed border test after changes to normal vec

--- tests/test_border.py
+++ tests/test_border.py
@@ -3,12 +3,16 @@ import pytest
 
 from src.model.border.border import Border
 from src.model.point.point import Point
+from src.model.cells.triangle import Triangle
+
 
 def test_border_vector():
-    p1 = Point((0,0))
-    p2 = Point((1,0))
+    p1 = Point((0, 0))
+    p2 = Point((1, 0))
+    p3 = Point((0, 1))
+    tri = Triangle([p1, p2, p3])
 
-    border = Border(p1, p2, neighbour=None)
+    border = Border(p1, p2, None, tri)
 
     v = p2.get_coordinates() - p1.get_coordinates()
     n = border.get_normal()
@@ -17,5 +21,3 @@ def test_border_vector():
     assert dot == pytest.approx(0.0)
 
 
-
-    
