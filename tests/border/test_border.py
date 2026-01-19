dd64559a09e79304b00ebf01065f94a906dc9bc6
Fredrik Tveter <fredrik.tveter@nmbu.no>
2026-01-19 13:23:40 +0100

    feat: finished border test and parameterized point

--- tests/border/test_border.py
+++ tests/border/test_border.py
@@ -4,45 +4,70 @@ from src.model.border.border import Border
 from src.model.point.point import Point
 from src.model.cells.triangle import Triangle
 
-
-test_data = [([0, 0], [1, 0], [0, 1]),
-             ([0, 1], [0, 0], [1, 0]),
-             ([1, 0], [0, 1], [0, 0]),
-             ([0.3, 0.3], [0.1, 0.8], [0.5, 0.5]),
-             ([0.5, 0.5], [0.3, 0.3], [0.1, 0.8]),
-             ([0.1, 0.8], [0.5, 0.5], [0.3, 0.3])]
+# points in 2 triangles, where the points move location at a time
+test_data = [([0, 0], [1, 0], [0, 1], 1),  # p1, p2, p3
+             ([0, 1], [0, 0], [1, 0], 2),  # p3, p1, p2
+             ([1, 0], [0, 1], [0, 0], 3),  # p2, p3, p1
+             # using floats:
+             ([0.3, 0.3], [0.1, 0.8], [0.5, 0.5], 4),
+             ([0.5, 0.5], [0.3, 0.3], [0.1, 0.8], 5),
+             ([0.1, 0.8], [0.5, 0.5], [0.3, 0.3], 6)]
 
 
 @pytest.fixture(params=test_data)
 def create_Borders(request):
-    point1, point2, point3 = request.param
+    """
+    parameterized fixture to create the borders with all the
+    points, normals, and coresponding triangle.
+    """
+    point1, point2, point3, neigbour = request.param
     p1 = Point(point1)
     p2 = Point(point2)
     p3 = Point(point3)
     tri = Triangle([p1, p2, p3])
-    border = Border(p1, p2, None, tri)
+    border = Border(p1, p2, neigbour, tri)
     v = p2.get_coordinates() - p1.get_coordinates()
     n = border.get_normal()
-    return n, v, border, tri, p1
+    return n, v, border, tri, [p1, p2, p3], neigbour
 
 
 def test_border_normal(create_Borders):
-    n, v, border, tri, p1 = create_Borders
+    """
+    Test to check if normal matches the current edge and that it is a normal
+    """
+    n, v, *_ = create_Borders
     dot = np.dot(v, n)
     assert dot == pytest.approx(0.0)
 
 
 def test_normal_length(create_Borders):
-    n, v, border, tri, p1 = create_Borders
+    """
+    Test to check if the length of the normal vector matches the side length
+    """
+    n, v, *_ = create_Borders
     len_v = np.sqrt(v[0]**2+v[1]**2)
     len_n = np.sqrt(n[0]**2+n[1]**2)
     assert len_v == pytest.approx(len_n)
 
 
-def test_normal_dir(create_Borders):
-    n, v, border, tri, p1 = create_Borders
+def test_normal_direction(create_Borders):
+    """
+    Test to check if the normal vector is facing out of the triangle
+    """
+    n, _, _, tri, points, _ = create_Borders
+    p1 = points[0]
     tri_mid = tri.get_midpoint()
     mid_p1 = p1.get_coordinates() - tri_mid
     theta = np.arccos(np.inner(n, mid_p1) /
                       (np.linalg.norm(mid_p1)*np.linalg.norm(n)))
     assert theta < np.pi/2
+
+
+def test_neighbours(create_Borders):
+    """
+    Test to check if the correct neighbour gets returned
+    """
+    result = create_Borders
+    border = result[2]
+    neighbour = result[5]
+    assert border.get_neighbour() == neighbour
--- tests/point/test_point.py
+++ tests/point/test_point.py
@@ -3,25 +3,37 @@ import numpy as np
 
 from src.model.point.point import Point
 
+# chosen few point coordinates that include int, float and negative numbers
+test_data = [[1, 2], [2, 1], [0.2, 0.1], [1, 1], [-1, 1], [-0.5, -0.1]]
 
-def test_point_equality():
-    p1 = Point((1, 2))
-    p2 = Point((1, 2))
+
+@pytest.mark.parametrize('point', test_data)
+def test_point_equality(point):
+    """
+    Test equality function in Point class
+    """
+    p1 = Point(point)
+    p2 = Point(point)
 
     assert p1 == p2
 
 
-def test_point_hashing():
-    p1 = Point((1, 2))
-    p2 = Point((1, 2))
+@pytest.mark.parametrize('point', test_data)
+def test_point_hashing(point):
+    """
+    Test hashing function in Point class
+    """
+    p1 = Point(point)
+    p2 = Point(point)
 
     s = {p1, p2}
 
     assert len(s) == 1
 
 
-def test_point_get_coordinates():
-    p = Point((3, 4))
-    coord = p.get_coordinates()      
+@pytest.mark.parametrize('point', test_data)
+def test_point_get_coordinates(point):
+    p = Point(point)
+    coord = p.get_coordinates()
 
-    assert np.array_equal(coord, np.array([3, 4]))
+    assert np.array_equal(coord, np.array(point))
