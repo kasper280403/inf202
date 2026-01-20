ab0eba2165f57edc1a192e97095afe04471c2cd7
kasper280403 <kasper.s.karlsen@gmail.com>
2026-01-20 10:42:17 +0100

    feat: Added terminal options

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
 import pathlib
+import argparse
 import time
 import tomllib
 from src.controller import Controller
 
-def create_folder(base_name):
-    result_path = pathlib.Path(__file__).parent / "results"
 
-    i = 0
-    while True:
-        folder_name = base_name if i == 0 else f"{base_name}{i}"
-        folder_path = result_path / str(folder_name)
-        if not folder_path.exists():
-            break
-        i += 1
-    folder_path.mkdir(parents=True, exist_ok=False)
-    print(f"Simulation saved under results/{folder_name}")
+def main():
+    args = parse_args()
 
-    return folder_path
+    if args.folder:
+        search_directory = pathlib.Path(args.folder)
+    else:
+        search_directory = pathlib.Path(__file__).parent / "toml_files"
+
+    if args.find_all:
+        for config_file in search_directory.glob("*.toml"):
+            run_file(config_file)
+    elif args.config_file:
+        run_file(pathlib.Path(args.config_file))
+    else:
+        run_file(pathlib.Path("input.toml"))
+
+
+
+
+def run_file(toml_file):
+    with toml_file.open("rb") as f:
+        config = tomllib.load(f)
+
+        print("")
+        print("Running simulation with toml file:", toml_file.name)
+
+        n_steps = config["settings"]["nSteps"]
+        time_end = config["settings"]["tEnd"]
+
+        mesh_name = config["geometry"]["meshName"]
+        borders = config["geometry"]["borders"]
+
+        log_name = config["IO"].get("logName")
+        write_frequency = config["IO"].get("writeFrequency")
+
+        run_simulation(n_steps, time_end, mesh_name, borders, write_frequency, log_name)
+
 
 
 def run_simulation(n_steps, time_end, mesh_name, borders, write_frequency = None, log_name = None, center_point=None):
@@ -45,25 +70,47 @@ def run_simulation(n_steps, time_end, mesh_name, borders, write_frequency = None
         controller.make_video(log_folder_path, time_end)
     controller.create_image("final_image", f"time: {time_end:.2f}", log_folder_path)
 
+def create_folder(base_name):
+    result_path = pathlib.Path(__file__).parent / "results"
 
-toml_dir = pathlib.Path(__file__).parent / "toml_files"
+    i = 0
+    while True:
+        folder_name = base_name if i == 0 else f"{base_name}{i}"
+        folder_path = result_path / str(folder_name)
+        if not folder_path.exists():
+            break
+        i += 1
+    folder_path.mkdir(parents=True, exist_ok=False)
+    print(f"Simulation saved under results/{folder_name}")
 
-for toml_file in toml_dir.glob("*.toml"):
-    with toml_file.open("rb") as f:
-        config = tomllib.load(f)
+    return folder_path
 
-        print("")
-        print("Running simulation with toml file:", toml_file.name)
 
-        n_steps = config["settings"]["nSteps"]
-        time_end = config["settings"]["tEnd"]
+def parse_args():
+    parser = argparse.ArgumentParser()
 
-        mesh_name = config["geometry"]["meshName"]
-        borders = config["geometry"]["borders"]
+    parser.add_argument(
+        "--find_all",
+        action="store_true",
+        help="Finds and runs all .toml files. "
+             "If folder is not specified, toml_files is used."
+    )
 
-        log_name = config["IO"].get("logName")
-        write_frequency = config["IO"].get("writeFrequency")
+    parser.add_argument(
+        "-f", "--folder",
+        metavar="FOLDER",
+        help="Folder to search for config files"
+    )
 
-        run_simulation(n_steps, time_end, mesh_name, borders, write_frequency, log_name)
+    parser.add_argument(
+        "-c", "--config_file",
+        metavar="FILE",
+        help="Read a specific config file. "
+             "If not specified, defaults to input.toml."
+    )
+
+    return parser.parse_args()
+
+
 
 
