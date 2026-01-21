fc0fae6d9d6c55a929f1cdb29c3c9fdd85caac9b
kasper280403 <kasper.s.karlsen@gmail.com>
2026-01-21 11:12:29 +0100

    docs: Finalized border.py

--- src/model/border/border.py
+++ src/model/border/border.py
@@ -7,14 +7,13 @@ class Border:
     Border class to represent the 3 borders of a triangle
 
     Attributes:
-        p1 (Point): first edgepoint of the border
-        p2 (Point): second edgepoint of the border
-        triangle (Triangle): triangle to which the border belongs
-        neighbour (Triangle): triangle to which the border borders
-        normal (np.array[float]): vector defining the normal facing
-                               outwards of the triangle for this border
-
+        _p1 (Point): first edgepoint of the border
+        _p2 (Point): second edgepoint of the border
+        _triangle (Triangle): triangle to which the border belongs
+        _neighbour (Cell): Cell to which the triangle borders, can be Edge or Triangle
+        _normal (np.array[float]): nomral vector facing outwards of the triangle on this edge
     """
+
     def __init__(self, p1, p2, neighbour=None, tri=None):
         self._p1 = p1
         self._p2 = p2
@@ -26,23 +25,20 @@ class Border:
 
     def calculate_normal(self):
         """
-        Calculates the normal for the current border
-        and makes sure it faces outwards
+        Calculates the normal and chooses direction outwards
+
+        Return:
+            np.array[float]: normal vector facing outwards of the border
         """
-        # Line from p1 to p2
+
         line_vec = self._p2.get_coordinates() - self._p1.get_coordinates()
-
-        # Create normal by crossing line vector with vector pointing in z dir
         normal = np.cross(line_vec, [0, 0, 1])[0:2]
-
-        # Line from triangle midpoint to p1
         midt_p1 = self._p1.get_coordinates() - self._triangle.get_midpoint()
-
-        # Check if normal is pointing out of triangle
         theta = np.arccos(np.inner(normal, midt_p1) /
                           (np.linalg.norm(normal) * np.linalg.norm(midt_p1)))
         if theta > np.pi / 2:
             normal = normal * (-1)
+
         return normal
 
     def get_points(self):
@@ -56,6 +52,6 @@ class Border:
 
     def create_edge(self):
         """
-        Create edge neigbour if no triangle neighbour
+        Creates Edge neigbour if the Border is created without Triangle neighbour
         """
         self._neighbour = Edge([self._p1, self._p2])
